dd39ca4b0938f0cfc6996bc6ad3d71015c6a1958
Lukin Kratas <lukin.kratas@seznam.cz>
2025-12-15 17:56:09 +0100

    add routers

--- zapisstavyapi/main.py
+++ zapisstavyapi/main.py
@@ -1,18 +1,6 @@
 from fastapi import FastAPI
-from zapisstavyapi.models.reading import Reading, ReadingIn
+from zapisstavyapi.routers.reading import router as reading_router
 
 app = FastAPI()
 
-readings = {}
-
-@app.post('/reading', response_model=Reading)
-async def create_reading(reading: ReadingIn) -> dict[str, str]:
-    data = reading.dict()
-    last_reading_id = len(readings)
-    new_reading = {**data, 'sensor_id': last_reading_id}
-    readings[last_reading_id] = new_reading
-    return new_reading
-
-@app.get('/reading', response_model=list[Reading])
-async def get_all_readings():
-    return list(readings.values())
+app.include_router(reading_router)
---
+++ zapisstavyapi/routers/reading.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+
+from zapisstavyapi.models.reading import Reading, ReadingIn
+
+router = APIRouter()
+readings = {}
+
+@router.post('/reading', response_model=Reading)
+async def create_reading(reading: ReadingIn) -> dict[str, str]:
+    data = reading.dict()
+    last_reading_id = len(readings)
+    new_reading = {**data, 'sensor_id': last_reading_id}
+    readings[last_reading_id] = new_reading
+    return new_reading
+
+@router.get('/reading', response_model=list[Reading])
+async def get_all_readings():
+    return list(readings.values())
