6ac6c82881e09c713d268008e374d734903f6b06
Lukin Kratas <47260673+lukinkratas@users.noreply.github.com>
2026-05-11 19:24:28 +0200

    add exceptions

---
+++ api/exceptions.py
@@ -0,0 +1,21 @@
+from fastapi import HTTPException, status
+
+credentials_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Invalid credentials",
+    headers={"WWW-Authenticate": "Bearer"},
+)
+
+token_exception = HTTPException(
+    status_code=status.HTTP_401_UNAUTHORIZED,
+    detail="Invalid token",
+    headers={"WWW-Authenticate": "Bearer"},
+)
+
+user_exists_exception = HTTPException(status_code=409, detail="User already exists")
+
+user_not_found_exception = HTTPException(status_code=404, detail="User not found")
+
+location_exists_exception = HTTPException(status_code=409, detail="Location already exists")
+
+location_not_found_exception = HTTPException(status_code=404, detail="Location not found")
