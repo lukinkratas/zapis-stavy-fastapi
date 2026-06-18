d7855b0169397dd1d30b1ea824603df83c3c0984
Lukin Kratas <47260673+lukinkratas@users.noreply.github.com>
2026-06-18 12:53:23 +0200

    update sql script

--- sql/init.sql
+++ sql/init.sql
@@ -6,17 +6,17 @@
 
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT uuidv7(),
-    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     email TEXT NOT NULL UNIQUE,
     password_hash TEXT NOT NULL,
-    confirmed BOOLEAN NOT NULL DEFAULT FALSE
+    confirmed BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
     -- role public.userrole NOT NULL DEFAULT 'STANDARD',
 );
 
 CREATE TABLE locations (
     id UUID PRIMARY KEY DEFAULT uuidv7(),
-    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     user_id UUID NOT NULL REFERENCES users (id) ON DELETE CASCADE,
     location_name TEXT NOT NULL,
-    CONSTRAINT unique_location_name UNIQUE (user_id, location_name)
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+    CONSTRAINT unique_location_name_per_user_id UNIQUE (user_id, location_name)
 );
